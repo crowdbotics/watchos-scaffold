46a83d30c5e7c2efe81dcd33566f02edca6f827d
Dan DeFelippi <dan@driverdan.com>
2018-10-23 16:24:59 -0500

    Initial commit

---
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}} WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  CrowdboticsWatchOS WatchKit Extension
+//
+//  Created by Dan on 10/23/18.
+//  Copyright © 2018 Crowdbotics. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
